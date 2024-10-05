1a53b1f29e9b172b3efb379a933fe5212e030b04
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-10-05 10:35:46 -0700

    update frontend config

--- client/src/lib/rrConfig.alias.ts
+++ client/src/lib/rrConfig.alias.ts
@@ -1,4 +1,12 @@
-export type RankingGroupNames = 'devs-24' | 'designers-24' | 'numbers'
+export type RankingGroupNames =
+    | 'devs-24'
+    | 'designers-24'
+    | 'numbers'
+    | 'dev-24-co28'
+    | 'dev-24-co27'
+    | 'dev-24-co26'
+    | 'des-24-co28'
+    | 'des-24-co27'
 
 export const rrConfig = {
     kFactor: 32,
@@ -33,9 +41,80 @@ export const rrConfig = {
                 'resume'
             ]
         },
-        numbers: {
-            questionOrder: ['Number 1', 'Number 2', 'Number 3'],
-            fieldOrder: []
+        'dev-24-co28': {
+            questionOrder: ['socialGood', 'technicalSkills', 'preference'],
+            fieldOrder: [
+                'pronouns',
+                'gradYear',
+                'majors',
+                'minors',
+                'whyNova',
+                'problem',
+                'community',
+                'links',
+                'additionalInfo',
+                'resume'
+            ]
+        },
+        'dev-24-co27': {
+            questionOrder: ['socialGood', 'technicalSkills', 'preference'],
+            fieldOrder: [
+                'pronouns',
+                'gradYear',
+                'majors',
+                'minors',
+                'whyNova',
+                'problem',
+                'community',
+                'links',
+                'additionalInfo',
+                'resume'
+            ]
+        },
+        'dev-24-co26': {
+            questionOrder: ['socialGood', 'technicalSkills', 'preference'],
+            fieldOrder: [
+                'pronouns',
+                'gradYear',
+                'majors',
+                'minors',
+                'whyNova',
+                'problem',
+                'community',
+                'links',
+                'additionalInfo',
+                'resume'
+            ]
+        },
+        'des-24-co28': {
+            questionOrder: ['socialGood', 'technicalSkills', 'preference'],
+            fieldOrder: [
+                'pronouns',
+                'gradYear',
+                'majors',
+                'minors',
+                'whyNova',
+                'problem',
+                'community',
+                'links',
+                'additionalInfo',
+                'resume'
+            ]
+        },
+        'des-24-co27': {
+            questionOrder: ['socialGood', 'technicalSkills', 'preference'],
+            fieldOrder: [
+                'pronouns',
+                'gradYear',
+                'majors',
+                'minors',
+                'whyNova',
+                'problem',
+                'community',
+                'links',
+                'additionalInfo',
+                'resume'
+            ]
         }
     },
 
@@ -62,13 +141,13 @@ export const rrConfig = {
                 name: 'socialGood',
                 question:
                     'Who demonstrates a bigger drive for impact and social good?',
-                weight: 0.1
+                weight: 0.3
             },
             {
                 name: 'technicalSkills',
                 question:
                     'Who is more technically skilled and better at problem solving?',
-                weight: 0.8
+                weight: 0.6
             },
             {
                 name: 'preference',
@@ -81,13 +160,108 @@ export const rrConfig = {
                 name: 'socialGood',
                 question:
                     'Who demonstrates a bigger drive for impact and social good?',
-                weight: 0.1
+                weight: 0.3
             },
             {
                 name: 'technicalSkills',
                 question:
                     'Who is more technically skilled and better at problem solving?',
-                weight: 0.8
+                weight: 0.6
+            },
+            {
+                name: 'preference',
+                question: 'Who would you prefer to work with?',
+                weight: 0.1
+            }
+        ],
+        'dev-24-co28': [
+            {
+                name: 'socialGood',
+                question:
+                    'Who demonstrates a bigger drive for impact and social good?',
+                weight: 0.3
+            },
+            {
+                name: 'technicalSkills',
+                question:
+                    'Who is more technically skilled and better at problem solving?',
+                weight: 0.6
+            },
+            {
+                name: 'preference',
+                question: 'Who would you prefer to work with?',
+                weight: 0.1
+            }
+        ],
+        'dev-24-co27': [
+            {
+                name: 'socialGood',
+                question:
+                    'Who demonstrates a bigger drive for impact and social good?',
+                weight: 0.3
+            },
+            {
+                name: 'technicalSkills',
+                question:
+                    'Who is more technically skilled and better at problem solving?',
+                weight: 0.6
+            },
+            {
+                name: 'preference',
+                question: 'Who would you prefer to work with?',
+                weight: 0.1
+            }
+        ],
+        'dev-24-co26': [
+            {
+                name: 'socialGood',
+                question:
+                    'Who demonstrates a bigger drive for impact and social good?',
+                weight: 0.3
+            },
+            {
+                name: 'technicalSkills',
+                question:
+                    'Who is more technically skilled and better at problem solving?',
+                weight: 0.6
+            },
+            {
+                name: 'preference',
+                question: 'Who would you prefer to work with?',
+                weight: 0.1
+            }
+        ],
+        'des-24-co28': [
+            {
+                name: 'socialGood',
+                question:
+                    'Who demonstrates a bigger drive for impact and social good?',
+                weight: 0.3
+            },
+            {
+                name: 'technicalSkills',
+                question:
+                    'Who is more technically skilled and better at problem solving?',
+                weight: 0.6
+            },
+            {
+                name: 'preference',
+                question: 'Who would you prefer to work with?',
+                weight: 0.1
+            }
+        ],
+        'des-24-co27': [
+            {
+                name: 'socialGood',
+                question:
+                    'Who demonstrates a bigger drive for impact and social good?',
+                weight: 0.3
+            },
+            {
+                name: 'technicalSkills',
+                question:
+                    'Who is more technically skilled and better at problem solving?',
+                weight: 0.6
             },
             {
                 name: 'preference',
@@ -97,6 +271,131 @@ export const rrConfig = {
         ]
     },
     systems: {
+        'dev-24-co28': {
+            numRounds: 4,
+            rounds: [
+                {
+                    number: 0,
+                    keepPercentage: 1,
+                    numPivots: 3
+                },
+                {
+                    number: 1,
+                    keepPercentage: 0.75,
+                    numPivots: 3
+                },
+                {
+                    number: 2,
+                    keepPercentage: 0.5,
+                    numPivots: 2
+                },
+                {
+                    number: 3,
+                    keepPercentage: 0.25,
+                    numPivots: 2
+                }
+            ]
+        },
+        'dev-24-co27': {
+            numRounds: 4,
+            rounds: [
+                {
+                    number: 0,
+                    keepPercentage: 1,
+                    numPivots: 3
+                },
+                {
+                    number: 1,
+                    keepPercentage: 0.75,
+                    numPivots: 3
+                },
+                {
+                    number: 2,
+                    keepPercentage: 0.5,
+                    numPivots: 2
+                },
+                {
+                    number: 3,
+                    keepPercentage: 0.25,
+                    numPivots: 2
+                }
+            ]
+        },
+        'dev-24-co26': {
+            numRounds: 4,
+            rounds: [
+                {
+                    number: 0,
+                    keepPercentage: 1,
+                    numPivots: 3
+                },
+                {
+                    number: 1,
+                    keepPercentage: 0.75,
+                    numPivots: 3
+                },
+                {
+                    number: 2,
+                    keepPercentage: 0.5,
+                    numPivots: 2
+                },
+                {
+                    number: 3,
+                    keepPercentage: 0.25,
+                    numPivots: 2
+                }
+            ]
+        },
+        'des-24-co28': {
+            numRounds: 4,
+            rounds: [
+                {
+                    number: 0,
+                    keepPercentage: 1,
+                    numPivots: 3
+                },
+                {
+                    number: 1,
+                    keepPercentage: 0.75,
+                    numPivots: 3
+                },
+                {
+                    number: 2,
+                    keepPercentage: 0.5,
+                    numPivots: 2
+                },
+                {
+                    number: 3,
+                    keepPercentage: 0.25,
+                    numPivots: 1
+                }
+            ]
+        },
+        'des-24-co27': {
+            numRounds: 4,
+            rounds: [
+                {
+                    number: 0,
+                    keepPercentage: 1,
+                    numPivots: 3
+                },
+                {
+                    number: 1,
+                    keepPercentage: 0.75,
+                    numPivots: 3
+                },
+                {
+                    number: 2,
+                    keepPercentage: 0.5,
+                    numPivots: 2
+                },
+                {
+                    number: 3,
+                    keepPercentage: 0.25,
+                    numPivots: 1
+                }
+            ]
+        },
         numbers: {
             numRounds: 1,
             rounds: [
@@ -123,62 +422,62 @@ export const rrConfig = {
             ]
         },
         'devs-24': {
-            numRounds: 2,
+            numRounds: 5,
             rounds: [
                 {
                     number: 0,
                     keepPercentage: 1,
-                    numPivots: 5
+                    numPivots: 10
                 },
                 {
                     number: 1,
                     keepPercentage: 0.85,
-                    numPivots: 1
+                    numPivots: 8
                 },
                 {
                     number: 2,
                     keepPercentage: 0.65,
-                    numPivots: 5
+                    numPivots: 6
                 },
                 {
                     number: 3,
                     keepPercentage: 0.45,
-                    numPivots: 5
+                    numPivots: 4
                 },
                 {
                     number: 4,
                     keepPercentage: 0.25,
-                    numPivots: 5
+                    numPivots: 2
                 }
             ]
         },
         'designers-24': {
-            numRounds: 2,
+            numRounds: 5,
             rounds: [
                 {
                     number: 0,
                     keepPercentage: 1,
-                    numPivots: 1
+                    numPivots: 10
                 },
                 {
                     number: 1,
                     keepPercentage: 0.85,
-                    numPivots: 1
+                    numPivots: 8
                 },
                 {
                     number: 2,
                     keepPercentage: 0.65,
-                    numPivots: 1
+                    numPivots: 6
                 },
                 {
                     number: 3,
                     keepPercentage: 0.45,
-                    numPivots: 1
+                    numPivots: 4
                 },
                 {
                     number: 4,
                     keepPercentage: 0.25,
-                    numPivots: 1
+                    numPivots: 2
                 }
             ]
         }
